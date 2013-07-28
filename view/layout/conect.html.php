cee585ec250dd8cfb4fe20eda05fad79f131bc79
evangelion62 <sebastien.ronger.evangelion@gmail.com>
2013-07-28 22:02:01 +0200

    module de connection
    léger changemet de la vue:
    *espacement reduit entre les deux type submit

--- view/layout/conect.html.php
+++ view/layout/conect.html.php
@@ -1,15 +1,15 @@
 <li class="conect"><a href="?controller=home">Espace membre</a>
     <div class="subnav">
 		<form class="conect" method="post" action="index.php?controller=user&action=conect">
-			<p>
+			<p style="padding-bottom: 0px;background-image: ">
 			 	<input type="text" name="login" id="login" />
 			 	<input type="password" name="password" id="password" />
-			 	<input type="submit" value="Connexion" />
+			 	<input style="background-image:none;" type="submit" value="Connexion" />
 			 </p>
 		</form>
 		<form class="conect" method="post" action="index.php?controller=user&action=addUser">
-			<p>
-			 	<input type="submit" value="Inscription" />
+			<p style="padding-top: 0px;">
+			 	<input style="background-image:none;" type="submit" value="Inscription" />
 			 </p>
 		</form> 					
     </div>
