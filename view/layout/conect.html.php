cec2aa5556186fafcdd128ceb53013def8db9daf
evangelion62 <sebastien.ronger.evangelion@gmail.com>
2013-07-28 21:30:49 +0200

    module de connection
    travaille sur la vue
    *formulaire
    *liens

--- view/layout/conect.html.php
+++ view/layout/conect.html.php
@@ -1,11 +1,15 @@
 <li class="conect"><a href="?controller=home">Espace membre</a>
     <div class="subnav">
-		<form class="conect" method="post" action="">
+		<form class="conect" method="post" action="index.php?controller=user&action=conect">
 			<p>
 			 	<input type="text" name="login" id="login" />
 			 	<input type="password" name="password" id="password" />
 			 	<input type="submit" value="Connexion" />
-			 	<input type="button" value="Inscription" />
+			 </p>
+		</form>
+		<form class="conect" method="post" action="index.php?controller=user&action=addUser">
+			<p>
+			 	<input type="submit" value="Inscription" />
 			 </p>
 		</form> 					
     </div>
