013f97c26284a9af5cbd22c6140a7661433dc631
Skartius <v.bredmayeux@gmail.com>
2013-07-28 15:49:48 +0200

    Maj
    Majuscules sur les 1er mots du formulaire dans la vue adduser.

--- view/user/addUser.html.php
+++ view/user/addUser.html.php
@@ -2,16 +2,16 @@
 <div class="form">
 	<form action="" method="post">
 		<p>
-			<label for="identifiant">identifiant:</label>
+			<label for="identifiant">Identifiant:</label>
 			<input type="text" name="identifiant" id="identifiant"/>
 			<br/>
-			<label for="email">adresse e-mail:</label>
+			<label for="email">Adresse e-mail:</label>
 			<input type="email" name="email" id="email"/>
 			<br/>
-			<label for="passwordSelect">mot de passe:</label>
+			<label for="passwordSelect">Mot de passe:</label>
 			<input type="password" name="passwordSelect" id="passwordSelect"/>
 			<br/>
-			<label for="passwordConfirm">confirmation du mot de passe:</label>
+			<label for="passwordConfirm">Confirmation du mot de passe:</label>
 			<input type="password" name="passwordConfirm" id="passwordConfirm"/>
 			<br/>
 			<input type="submit" value="Envoyer"/>
