5b8d2da4a50f8ce14043aa17a2e04caa4f675d8e
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-11-04 15:02:15 +0100

    config : principale -> création
    création du fichier de configuration principale

---
+++ conf/conf.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * auto chargement de classe utilisant les namespace 
+ */
+function autoload($class)
+{
+	$classFile = str_replace('\\', '/', $class).'.php';
+	if (is_file($classFile)){
+		require $classFile;
+	}
+}
+
+spl_autoload_register('autoload');
+
+/*
+ * conection à la bdd
+ */
+
+/*
+ * demarrage des session
+ */
+ 
+/*
+ * definition des varriable $controller et $action
+ */
+$Controller = (empty($_GET['controller'])) ? '\controler\Index' : '\controller\\'.ucfirst($_GET['controller']);
+$action = (empty($_GET['action'])) ? 'default' : $_GET['action'];
