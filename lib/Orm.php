d7936897055f9b6628fa1068471c91fac1fd7a70
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-11-15 05:00:55 +0100

    orm

--- lib/Orm.php
+++ lib/Orm.php
@@ -1,6 +1,50 @@
 <?php
 namespace lib;
 
-abstract class Orm {
+final class Orm {
+	/**********************************construction****************************************************************************/
+	protected $_db;
 	
+	public function __construct(PDO $db) {
+		$this->setDb($db);
+	}
+	
+	public function setDb(PDO $db)
+	{
+		$this->_db = $db;
+	}
+	
+	/*********************************méthode sur les tables*******************************************************************/
+	public function tableCreat(Entity $entity){
+		
+	}
+	
+	public function tableTruncate(Entity $entity){
+		
+	}
+	
+	public function tableDrop(Entity $entity){
+		
+	}
+	
+	public function tableSave(Entity $entity){
+		
+	}
+	
+	/*********************************méthode sur les entity*******************************************************************/
+	public function add(Entity $entity) {
+		;
+	}
+	
+	public function get() {
+		;
+	}
+	
+	public function update(Entity $entity) {
+		;
+	}
+	
+	public function delete(){
+		
+	}
 }
